b3e5d7b3c44b458743c5ce5a3c9a6bd25c75e8af
rifaldibpn2 <rifaldibpn2@gmail.com>
2023-06-02 14:13:58 +0700

    Controller

--- api-library/controllers/bookController/bookController.go
+++ api-library/controllers/bookController/bookController.go
@@ -1,21 +1,26 @@
 package bookcontroller
 
-func Index() {
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/indrabpn12/Rest_API_Test.git/models"
+	"gorm.io/gorm"
+)
+
+func Index(c *gin.Context) {
+}
+
+func Show(c *gin.Context) {
 
 }
 
-func Store() {
+func Store(c *gin.Context) {
+}
+
+func Update(c *gin.Context) {
 
 }
 
-func Show() {
-
-}
-
-func Edit() {
-
-}
-
-func Destroy() {
-
+func Destroy(c *gin.Context) {
 }
--- api-library/main.go
+++ api-library/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	bookcontroller "github.com/indrabpn12/FinalProjectGolang.git/controllers/bookController"
+	"github.com/indrabpn12/FinalProjectGolang.git/models"
+
 	"github.com/gin-gonic/gin"
-	"github.com/indrabpn12/FinalProjectGolang.git/controllers/bookcontroller"
 )
 
 func main() {
 	r := gin.Default()
 
+	models.ConnectDatabase()
+
 	r.GET("/books", bookcontroller.Index)
-	r.POST("/books", bookcontroller.Store)
-	r.GET("/books/:id", bookcontroller.Show)
-	r.PUT("/books/:id", bookcontroller.Edit)
-	r.DELETE("/books/:id", bookcontroller.Destroy)
 
 	r.Run()
 }
