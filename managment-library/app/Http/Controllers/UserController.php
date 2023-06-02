924d4071fd49a5fa838ce97787954354f80a08e3
rifaldibpn2 <rifaldibpn2@gmail.com>
2023-06-02 20:54:36 +0700

    Back End Done

---
+++ managment-library/app/Http/Controllers/UserController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    //
+
+    public function createUsers(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users,email',
+            'role' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'role' => $request->role,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'User created successfully', 'user' => $user]);
+    }
+
+    public function getUsers()
+    {
+        $users = User::all();
+
+        return response()->json(['users' => $users]);
+    }
+
+    public function updateUser(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'role' => 'required',
+            'email' => 'required',
+        ]);
+
+        $user = User::find($id);
+
+        $user->update([
+            'name' => $request->name,
+            'role' => $request->role,
+            'email' => $request->email,
+        ]);
+
+        return response()->json(['message' => 'User updated successfully', 'user' => $user]);
+    }
+
+    public function deleteUser($id)
+    {
+        $user = User::find($id);
+
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted successfully']);
+    }
+
+    public function getUser($id)
+    {
+        $user = User::find($id);
+
+        return response()->json(['user' => $user]);
+    }
+
+    public function changePassword(Request $request, $id)
+    {
+        $request->validate([
+            'password' => 'required',
+        ]);
+
+        $user = User::find($id);
+
+        $user->update([
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'Password changed successfully']);
+    }
+}
--- managment-library/routes/api.php
+++ managment-library/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\BorrowController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,22 +22,37 @@ use App\Http\Controllers\CategoryController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+//Book API
 Route::get("/books", [BookController::class, 'getBook']);
 Route::get("/books/{id}", [BookController::class, 'getBookById']);
 Route::post("/books", [BookController::class, 'addBook']);
 Route::put("/books/{id}", [BookController::class, 'updateBook']);
 Route::delete("/books/{id}", [BookController::class, 'deleteBook']);
 
+//Author API
 Route::get("/authors", [AuthorController::class, 'getAuthor']);
 Route::get("/authors/{id}", [AuthorController::class, 'getAuthorById']);
 Route::post("/authors", [AuthorController::class, 'addAuthor']);
 Route::put("/authors/{id}", [AuthorController::class, 'updateAuthor']);
 Route::delete("/authors/{id}", [AuthorController::class, 'deleteAuthor']);
 
-
+//Category API
 Route::get("/categories", [CategoryController::class, 'getCategory']);
 Route::get("/categories/{id}", [CategoryController::class, 'getCategoryById']);
 Route::post("/categories", [CategoryController::class, 'addCategory']);
 Route::put("/categories/{id}", [CategoryController::class, 'updateCategory']);
 Route::delete("/categories/{id}", [CategoryController::class, 'deleteCategory']);
+
+//Borrow API
+Route::get("/borrows", [BorrowController::class, 'getBorrow']);
+Route::get("/borrows/{id}", [BorrowController::class, 'getBorrowById']);
+Route::post("/borrows", [BorrowController::class, 'addBorrow']);
+Route::put("/borrows/{id}", [BorrowController::class, 'updateBorrow']);
+Route::delete("/borrows/{id}", [BorrowController::class, 'deleteBorrow']);
+
+//User API
+Route::get("/users", [UserController::class, 'getUsers']);
+Route::get("/users/{id}", [UserController::class, 'getUserById']);
+Route::post("/users", [UserController::class, 'addUser']);
+Route::put("/users/{id}", [UserController::class, 'updateUser']);
+Route::delete("/users/{id}", [UserController::class, 'deleteUser']);
