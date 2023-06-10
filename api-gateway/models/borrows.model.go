3106496b02088cc15e10db647cdca97fc9c099df
rifaldibpn2 <rifaldibpn2@gmail.com>
2023-06-10 15:07:00 +0700

    Borrow

--- api-gateway/models/borrows.model.go
+++ api-gateway/models/borrows.model.go
@@ -11,3 +11,13 @@ type Borrow struct {
 	Status      string `json:"status"`
 	Penalty     uint   `json:"penalty"`
 }
+
+type BorrowInput struct {
+	ID          uint   `json:"id" gorm:"primaryKey"`
+	User_id     uint   `json:"user_id"`
+	Book_id     uint   `json:"book_id"`
+	Borrow_date string `json:"borrow_date"`
+	Return_date string `json:"return_date"`
+	Status      string `json:"status"`
+	Penalty     uint   `json:"penalty"`
+}
